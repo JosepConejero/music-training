cab0ffbadca489ab2f0f636eb2b28c6f8e62958e
JosepConejero <bermeu@gmail.com>
2024-09-16 20:38:11 +0200

    Added IntervalsPage.jsx code and indervals.js

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - Icono del "play" en los botones de time
 
 ## Errores para corregir
+
+- distancia inexacta de la nota mostrada según la pantalla móvil / ordenador
+- from - to -> validaciones (si la6 la6, bucle infinito)
+- from - to -> cuando sale el foco se resetean los valores por defecto, no hay permanencia
--- src/pages/IntervalsPage.jsx
+++ src/pages/IntervalsPage.jsx
@@ -1,10 +1,80 @@
+import { useState } from "react";
 import { Navbar } from "../components/Navbar";
+import { intervals } from "../pagesHelpers/intervals";
+import { uniqueKey } from "../helpers/uniqueKey";
 
-export const IntervalsPage = () => {
+export const IntervalsPage = ({ currentInterval, currentIntervalAnswer, showNextIntervalAnswer }) => {
+  const [pressedAnswerButton, setPressedAnswerButton] = useState(false);
+  const [pressedShowTableButton, setPressedShowTableButton] = useState(false);
+
+  const showAnswer = () => {
+    setPressedAnswerButton((prevState) => !prevState);
+  };
+
+  const showIntervalTable = () => {
+    setPressedShowTableButton((prevState) => !prevState);
+  };
+
   return (
     <>
       <Navbar />
-      <div>Intervals Practice</div>
+      <div>
+        <div>
+          <p>Intervals Practice</p>
+        </div>
+
+        <div className="result-interval"></div>
+        <div>
+          <div className="interval-view-container">
+            <div className="interval-view-ask-box">
+              <p className="interval-view-ask-text" onClick={showAnswer}>
+                {currentInterval}
+              </p>
+            </div>
+            <div className="interval-view-answer-box">
+              <p className="interval-view-answer-text" onClick={showAnswer}>
+                {pressedAnswerButton ? currentIntervalAnswer : "?"}
+              </p>
+            </div>
+          </div>
+
+          <div className="interval-buttons-container">
+            <div className="interval-next-button-box">
+              <button className="interval-next-button" onClick={showNextIntervalAnswer}>
+                NEXT
+              </button>
+            </div>
+            <div className="interval-show-table-box">
+              <button className="interval-show-table-button" onClick={showIntervalTable}>
+                SHOW TABLE
+              </button>
+            </div>
+          </div>
+
+          <div className={pressedShowTableButton ? "interval-table-container" : "hidden-interval-table-container"}>
+            <div className="interval-table-column">
+              {Object.entries(intervals).map(
+                (interval, index) =>
+                  index < 22 && (
+                    <span key={uniqueKey}>
+                      `${interval[0]} - ${interval[0]}`
+                    </span>
+                  )
+              )}
+            </div>
+            <div className="interval-table-column">
+              {Object.entries(intervals).map(
+                (interval, index) =>
+                  index >= 22 && (
+                    <span key={uniqueKey}>
+                      `${interval[0]} - ${interval[0]}`
+                    </span>
+                  )
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

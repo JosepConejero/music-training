8f97ba6ad174253eff9af68fa1f1fac850fdbb2f
JosepConejero <bermeu@gmail.com>
2024-09-29 15:10:30 +0200

    Modified Staff, staffStyles

--- src/staffComponents/Staff.jsx
+++ src/staffComponents/Staff.jsx
@@ -6,15 +6,15 @@ import "./staffStyles.css";
 export const Staff = ({ activeNote }) => {
   return (
     <>
-      <div className="general">
-        <div className="caja">
-          {["sol6", "mi6", "do6", "la5", "fa5", "re5", "si4", "sol4", "mi4", "do4", "la3", "fa3", "re3"].map((note) => (
-            <StaffLine key={uniqueKey()} staffNoteLine={note} activeNote={activeNote} />
-          ))}
-        </div>
-
-        <div className="nota" style={{ position: "absolute", top: distancia[activeNote] }}></div>
+      {/* <div className="general"> */}
+      <div className="caja">
+        {["sol6", "mi6", "do6", "la5", "fa5", "re5", "si4", "sol4", "mi4", "do4", "la3", "fa3", "re3"].map((note) => (
+          <StaffLine key={uniqueKey()} staffNoteLine={note} activeNote={activeNote} />
+        ))}
       </div>
+
+      <div className="nota" style={{ position: "absolute", top: distancia[activeNote] }}></div>
+      {/* </div> */}
     </>
   );
 };

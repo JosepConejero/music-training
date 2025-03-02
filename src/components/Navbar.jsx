4f74908c49ce1e17fb62a82152a0317693b6d555
JosepConejero <bermeu@gmail.com>
2025-03-02 21:47:43 +0100

    Modified hamburger menu height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,22 @@ import { useState } from "react";
 
 export const Navbar = () => {
   const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
-    
+
   const toggleMenu = () => {
     setIsSideMenuVisible(!isSideMenuVisible);
   };
 
-   const hideSideMenu = () => {
+  const hideSideMenu = () => {
     setIsSideMenuVisible(false);
-  }; 
+  };
 
   return (
     <>
-     <SideMenu isSideMenuVisible={isSideMenuVisible} hideSideMenu={hideSideMenu}></SideMenu>
+      <SideMenu isSideMenuVisible={isSideMenuVisible} hideSideMenu={hideSideMenu}></SideMenu>
       <main className="navbar navbar-up">
-        <button className="hamburger" onClick={toggleMenu}><i className="fa fa-bars"></i></button>
+        <button className="hamburger" onClick={toggleMenu}>
+          <i className="fa fa-bars"></i>
+        </button>
         <Link to="/">MAIN</Link>
         <Link to="/staff-practice">STAFF</Link>
         <Link to="/intervals">INTERVALS</Link>

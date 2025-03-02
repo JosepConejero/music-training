b5073ef4a7344bd6d1e355601cbc1597b64d88a0
JosepConejero <bermeu@gmail.com>
2025-03-02 23:04:09 +0100

    Removed transitions in final classes

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import "./SideMenu.css";
+import { useEffect } from "react";
 
 export const SideMenu = ({ isSideMenuVisible, hideSideMenu }) => {
+  useEffect(() => {
+    console.log(isSideMenuVisible);
+  }, [isSideMenuVisible]);
+
   return (
     <>
       {isSideMenuVisible && (
         <>
           <div className="fondo" onClick={hideSideMenu}></div>
-          <div className="menu">
+          {/* <div className="menu"> */}
+          <div className={`menu ${isSideMenuVisible ? "expanded" : "merdaputa"}`}>
             <ul>
               <li onClick={hideSideMenu}>
                 <Link to="/">Main</Link>

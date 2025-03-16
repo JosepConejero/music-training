7483c75eaae874d1343f0f78ca083780ae6ae97b
JosepConejero <bermeu@gmail.com>
2025-03-16 21:14:02 +0100

    Modified useConfigurationModl for avoid the ESLint error

--- src/hooks/useConfigurationModal.js
+++ src/hooks/useConfigurationModal.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useEffect, useState, useRef } from "react";
 
 export const useConfigurationModal = (initialConfiguration, isConfigurationRight) => {
